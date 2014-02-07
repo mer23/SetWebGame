610e68f4495f739538752bea5605d657b2c4a2d1
mer23 <martinob23@mail.com>
2014-02-07 04:17:04 +0200

    Update Card.php

--- SetWebGame/Card.php
+++ SetWebGame/Card.php
@@ -27,6 +27,14 @@ class Card {
         return $card1->color == $card2->color && $card1->shape == $card2->shape &&
                 $card1->texture == $card2->texture && $card1->number == $card2->number;
     }
+    
+    public static function form_a_set($first_card, $second_card, $third_card) {
+        //TODO: copy from js code.
+    }
+    
+    public static function are_consistent_fields($field_one, $field_two, $field_three) {
+        //TODO: copy from js code.
+    }
 
     public function get_color() {
         return $this->color;
