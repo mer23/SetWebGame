6d39c2cafd4f5b6f173df80b30824e4bd1dd97f1
mer23 <martinob23@mail.com>
2014-02-07 02:19:36 +0200

    Update Board.php

--- SetWebGame/Board.php
+++ SetWebGame/Board.php
@@ -1,5 +1,5 @@
 <?php
-    include 'Deck.php';
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,16 +8,13 @@
 
 class Board {
     
-    private $deck;
     private $number_of_slots;
     
-    public function __construct($number_of_slots, $game_difficulty) {
-        $this->number_of_slots = $number_of_slots;
-        $this->deck = new Deck($game_difficulty);
-    }
+    private $slots;
     
-    public function get_deck() {
-        return $this->deck;
+    public function __construct($number_of_slots) {
+        
+        $this -> number_of_slots = $number_of_slots;
+        $this -> slots= array($this -> number_of_slots);
     }
-    
 }
