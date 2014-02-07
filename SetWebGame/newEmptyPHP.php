d08aabd70d394a77cb5d4472fc675e5bbe49aaf0
mer23 <martinob23@mail.com>
2014-02-07 02:46:55 +0200

    Update newEmptyPHP.php

--- SetWebGame/newEmptyPHP.php
+++ SetWebGame/newEmptyPHP.php
@@ -1,9 +1,9 @@
 <?php 
-include 'Board.php';
-$board = new Board(7, 4);
-$deck = $board->get_deck();
-$arr = $deck->return_cards();
-foreach ($arr as $card) {
+include 'Game.php';
+$game = new Game();
+$game -> init();
+
+foreach ($game -> deck -> get_cards() as $card) {
     if(!empty($card)) {
         echo $card->get_color() , "&nbsp;&nbsp;&nbsp;&nbsp;", $card->get_texture(), "&nbsp;&nbsp;&nbsp;&nbsp;", $card->get_shape(), "&nbsp;&nbsp;&nbsp;&nbsp;", $card->get_number(), "<br>";
     }
