2c56d03ac3b8e4675ffeb84357ee0cc6d58a0676
mer23 <martinob23@mail.com>
2014-02-07 17:36:43 +0200

    Update Card.php

--- SetWebGame/Card.php
+++ SetWebGame/Card.php
@@ -1,127 +1,66 @@
 <?php
 
-include 'Deck.php';
-include 'Board.php';
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Card {
 
-class Game {
-
-    private $game_difficulty;
-    private $slots_in_board;
-    private $deck;
-    private $board;
-
-    /* holds the current state of the game, options menu or match */
-    private $current_screen;
-
-    const DEFAULT_DIFFICULTY = 4;
-    const DEFAULT_SLOTS_IN_BOARD = 9;
-
-    /* ----------------------------------------------------------------------------- */
-    const OPTIONS_MENU = 1; /* the user didn't create the game yet */
-    const LIST_OF_MATCHES = 2; /* the user awaits for other players to join his game */
-    const GAME_TABLE = 3; /* the game has already started */
-
-    /* ----------------------------------------------------------------------------- */
-
-    public function _construct() {
-
-        $this->game_difficulty = Game::DEFAULT_DIFFICULTY;
-        $this->slots_in_board = Game::DEFAULT_SLOTS_IN_BOARD;
-
-        $this->current_screen = Game::OPTIONS_MENU;
-    }
-
-    //Updates this game's difficulty whenever the user changes its value.
-    public function set_difficulty($updated_difficulty) {
-
-        if ($this->current_screen == Game::OPTIONS_MENU) {
-            $this->game_difficulty = $updated_difficulty;
-        }
-    }
-
-    //Updates the number of slots at this game's board whenever the user changes its value.
-    public function set_slots_in_board($updated_slots_in_board) {
-
-        if ($this->current_screen == Game::OPTIONS_MENU) {
-            $this->slots_in_board = $updated_slots_in_board;
-        }
-    }
-
-    /* Retrieves this game's deck */
-
-    public function get_deck() {
-        if ($this->current_screen == Game::GAME_TABLE) {
-            return $this->deck;
-        }
-    }
-
-    /* Retrieves this game's board */
-
-    public function get_board() {
-        if ($this->current_screen == Game::GAME_TABLE) {
-            return $this->board;
-        }
-    }
-
-    /* Allocates a card from the deck on each empty slot at board */
-
-    public function fill_board() {
-
-        foreach ($this->get_board()->get_slots() as $slot) {
-            if (empty($slot)) {
-                $this->deal_card($slot);
-            }
-        }
-    }
-
-    /* Deals a card from the deck to the board (empty)slot passed as parameter. */
-
-    public function deal_card($empty_slot) {
-
-        $empty_slot = $this->get_deck()->draw_card();
-    }
+    //Field Color
+    public static $colors = ["yellow", "red", "blue"];
+    //Field Shape
+    public static $shapes = ["diamond", "stadium", "peanut"];
+    //Field Number
+    public static $numbers = ["1", "2", "3"];
+    //Field Texture
+    public static $textures = ["filled", "stripped", "empty"];
     
-    public function cards_to_deck() {
-        
-    }
-    
-    public function pause(){
-     /* This has to retreat all cards from board and put them back in deck.
-      * Then, the deck has to be shuffled. When unpaused, new cards will be dealt 
-      * at the board so the game can continue.
-      * Only one pause per user per match shall be allowed.
-      */ 
-    }
-    
-    public function unpause() {
-        
+    private $color;
+    private $shape;
+    private $number;
+    private $texture;
+
+    public function __construct($color, $shape, $number, $texture) {
+        $this->color = $color;
+        $this->number = $number;
+        $this->shape = $shape;
+        $this->texture = $texture;
     }
 
-    /* Passes to next screen */
-
-    public function create_match() {
-
-        if ($this->current_screen == Game::OPTIONS_MENU) {
-            $this->current_screen = Game::LIST_OF_MATCHES;
-        }
+    public static function are_equal_cards($card1, $card2) {
+        return $card1->color == $card2->color && $card1->shape == $card2->shape &&
+                $card1->texture == $card2->texture && $card1->number == $card2->number;
     }
 
-    /* Starts the game */
+    public static function form_a_set($first_card, $second_card, $third_card) {
 
-    public function init() {
-        if ($this->current_screen == Game::LIST_OF_MATCHES) {
+        return
+        are_consistent_fields($first_card->texture, $second_card->texture, $third_card->texture) &&
+        are_consistent_fields($first_card->color, $second_card->color, $third_card->color) &&
+        are_consistent_fields($first_card->shape, $second_card->shape, $third_card->shape) &&
+        are_consistent_fields($first_card->number, $second_card->number, $third_card->number);
+    }
 
-            $this->deck = new Deck($this->game_difficulty);
-            $this->board = new Board($this->slots_in_board);
-            $this->fill_board();
+    public static function are_consistent_fields($field_one, $field_two, $field_three) {
 
-            $this->current_screen = Game::GAME_TABLE;
-        }
+        return (field_one == field_two && field_two == field_three) ||
+                (field_one != field_two && field_two != field_three);
+    }
+
+    public function get_color() {
+        return $this->color;
+    }
+
+    public function get_shape() {
+        return $this->shape;
+    }
+
+    public function get_number() {
+        return $this->number;
+    }
+
+    public function get_texture() {
+        return $this->texture;
+    }
+
+    public function to_string() {
+        return $this->texture + " " + $this->color + " " + $this->shape + " " + $this->number;
     }
 
 }
