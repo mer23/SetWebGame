4bfd275dc9bb0ea64b1e08e3363cbd32b64317fb
mer23 <martinob23@mail.com>
2014-02-07 17:38:28 +0200

    Update Game.php

--- SetWebGame/Game.php
+++ SetWebGame/Game.php
@@ -18,38 +18,44 @@ class Game {
     /* holds the current state of the game, options menu or match */
     private $current_screen;
 
-    /*     * ********************************************************************** */
+    const DEFAULT_DIFFICULTY = 4;
+    const DEFAULT_SLOTS_IN_BOARD = 9;
 
-    /* value for current_screen when the user didn't start the game yet because he/she is still setting up the game options */
+    /* ----------------------------------------------------------------------------- */
+    const OPTIONS_MENU = 1; /* the user didn't create the game yet */
+    const LIST_OF_MATCHES = 2; /* the user awaits for other players to join his game */
+    const GAME_TABLE = 3; /* the game has already started */
 
-    const OPTIONS_MENU_SCREEN = 1;
-
-    /* value for current_screen when the user has already started the match */
-    const MATCH_SCREEN = 2;
-
-    /*     * ********************************************************************** */
+    /* ----------------------------------------------------------------------------- */
 
     public function _construct() {
 
-        $this->game_difficulty = 4;
-        $this->slots_in_board = 9;
-        $this->current_screen = Game::OPTIONS_MENU_SCREEN;
+        $this->game_difficulty = Game::DEFAULT_DIFFICULTY;
+        $this->slots_in_board = Game::DEFAULT_SLOTS_IN_BOARD;
+
+        $this->current_screen = Game::OPTIONS_MENU;
     }
 
     //Updates this game's difficulty whenever the user changes its value.
     public function set_difficulty($updated_difficulty) {
-        $this->game_difficulty = $updated_difficulty;
+
+        if ($this->current_screen == Game::OPTIONS_MENU) {
+            $this->game_difficulty = $updated_difficulty;
+        }
     }
 
     //Updates the number of slots at this game's board whenever the user changes its value.
     public function set_slots_in_board($updated_slots_in_board) {
-        $this->slots_in_board = $updated_slots_in_board;
+
+        if ($this->current_screen == Game::OPTIONS_MENU) {
+            $this->slots_in_board = $updated_slots_in_board;
+        }
     }
 
     /* Retrieves this game's deck */
 
     public function get_deck() {
-        if ($this->current_screen == Game::MATCH_SCREEN) {
+        if ($this->current_screen == Game::GAME_TABLE) {
             return $this->deck;
         }
     }
@@ -57,26 +63,65 @@ class Game {
     /* Retrieves this game's board */
 
     public function get_board() {
-        if ($this->current_screen == Game::MATCH_SCREEN) {
+        if ($this->current_screen == Game::GAME_TABLE) {
             return $this->board;
         }
     }
 
-    public function deal_cards() {
+    /* Allocates a card from the deck on each empty slot at board */
 
-        /* This function should check for empty slots on this game's board and fill them with 
-         * cards from this game's deck (in case there are cards left at the deck. 
-         * If not, it leaves the slots empty) */
+    public function fill_board() {
+
+        foreach ($this->get_board()->get_slots() as $slot) {
+            if (empty($slot)) {
+                $this->deal_card($slot);
+            }
+        }
+    }
+
+    /* Deals a card from the deck to the board (empty)slot passed as parameter. */
+
+    public function deal_card($empty_slot) {
+
+        $empty_slot = $this->get_deck()->draw_card();
+    }
+    
+    public function cards_to_deck() {
+        
+    }
+    
+    public function pause(){
+     /* This has to retreat all cards from board and put them back in deck.
+      * Then, the deck has to be shuffled. When unpaused, new cards will be dealt 
+      * at the board so the game can continue.
+      * Only one pause per user per match shall be allowed.
+      */ 
+    }
+    
+    public function unpause() {
+        
+    }
+
+    /* Passes to next screen */
+
+    public function create_match() {
+
+        if ($this->current_screen == Game::OPTIONS_MENU) {
+            $this->current_screen = Game::LIST_OF_MATCHES;
+        }
     }
 
     /* Starts the game */
 
     public function init() {
+        if ($this->current_screen == Game::LIST_OF_MATCHES) {
 
-        $this->deck = new Deck($this->game_difficulty);
-        $this->board = new Board($this->slots_in_board);
+            $this->deck = new Deck($this->game_difficulty);
+            $this->board = new Board($this->slots_in_board);
+            $this->fill_board();
 
-        $this->current_screen = Game::MATCH_SCREEN; //Updates state.
+            $this->current_screen = Game::GAME_TABLE;
+        }
     }
 
 }
