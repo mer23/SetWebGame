b15340175dcd5aa923e96c262d0978fbe3d97b7b
mer23 <martinob23@hotmail.com>
2014-02-09 23:54:48 +0200

    Update Board.php

--- SetWebGame/Board.php
+++ SetWebGame/Board.php
@@ -1,24 +1,31 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 class Board {
-    
+
     private $number_of_slots;
-    
     private $slots;
-    
+
     public function __construct($number_of_slots) {
-        
-        $this -> number_of_slots = $number_of_slots;
-        $this -> slots= array($this -> number_of_slots);
+
+        $this->number_of_slots = $number_of_slots;
+        $this->slots = array_fill(0, $this->number_of_slots, NULL);
     }
-    
+
     public function get_slots() {
         return $this->slots;
     }
+
+    public function card_to_slot($index, $card) {
+
+        if (empty($this->slots[$index])) {
+            $this->slots[$index] = $card;
+        }
+    }
+
 }
