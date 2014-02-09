e204756a9b91f92a6db83021795832d4c02898f4
mer23 <martinob23@hotmail.com>
2014-02-10 00:00:51 +0200

    Update Init.php

--- SetWebGame/Init.php
+++ SetWebGame/Init.php
@@ -1,33 +1,68 @@
 <?php
 
 include 'Game.php';
+
 $game = new Game();
-//$game -> set_difficulty(3);
+//$game->set_difficulty(3);
+$game->set_slots_in_board(12);
 $game->create_match();
+
 $game->init();
 
-//Used to show the proper message when comparing cards.
-$boolean_flag = false;
+$card= new Card(Card::get_colors()[1], Card::get_shapes()[1], Card::get_numbers()[1], Card::get_textures()[2] );
+$card2= new Card(Card::get_colors()[2], Card::get_shapes()[0], Card::get_numbers()[1], Card::get_textures()[1] );
+$card3= new Card(Card::get_colors()[1], Card::get_shapes()[2], Card::get_numbers()[0], Card::get_textures()[2] );
 
-//These two nested for loops compare cards in the deck to make sure they're all different.
-for ($i = 0; $i < sizeof($game->get_deck()->get_cards()) - 1; $i++) {
 
-    for ($j = $i + 1; $j < sizeof($game->get_deck()->get_cards()); $j++) {
-        
-        if (Card::are_equal_cards($game->get_deck()->get_cards()[$i], $game->get_deck()->get_cards()[$j])) {
-            echo 'HOW IS THAT TWO CARDS ARE EQUAL?!<br><br>';
-            $boolean_flag = true;
-        }
-    }
-}
-if (!$boolean_flag) {
-    echo "IT'S OK, THEY'RE ALL DIFFERENT!<br><br>";
+if (Card::are_consistent_fields($card->get_color(), $card2->get_color(), $card3->get_color())) {
+    echo 'TRUE';
 }
 
-/*Prints every card in the deck in a developer-readable format. 
- * Naor could have used the to_string() method to achieve this goal, but he likes to write a lot.*/
+
+/* _________________________ DECK DISPLAY _____________________________ */
+
+echo 'DECK: ', '(', sizeof($game->get_deck()->get_cards()), ' cards) <br><br>';
 foreach ($game->get_deck()->get_cards() as $card) {
     if (!empty($card)) {
+        //echo $card->to_string();
         echo $card->get_color(), "&nbsp;&nbsp;&nbsp;&nbsp;", $card->get_texture(), "&nbsp;&nbsp;&nbsp;&nbsp;", $card->get_shape(), "&nbsp;&nbsp;&nbsp;&nbsp;", $card->get_number(), "<br>";
+    } else {
+        echo 'EMPTY CARD!<br>';
     }
 }
+/* ____________________________________________________________________ */
+
+
+/* _________________________ BOARD DISPLAY ____________________________ */
+
+echo '<br><br>BOARD: ', '(', sizeof($game->get_board()->get_slots()), ' cards) <br><br>';
+
+foreach ($game->get_board()->get_slots() as $slot) {
+    if (!empty($slot)) {
+        //echo $card->to_string();
+        echo $slot->get_color(), "&nbsp;&nbsp;&nbsp;&nbsp;", $slot->get_texture(), "&nbsp;&nbsp;&nbsp;&nbsp;", $slot->get_shape(), "&nbsp;&nbsp;&nbsp;&nbsp;", $slot->get_number(), "<br>";
+    } else {
+        echo 'EMPTY CARD!<br>';
+    }
+}
+/* ____________________________________________________________________ */
+
+$game->is_there_set();
+
+//Used to show the proper message when comparing cards.
+//$boolean_flag = false;
+//These two nested for loops compare cards in the deck to make sure they're all different.
+
+    /* for ($i = 0; $i < sizeof($game->get_deck()->get_cards()) - 1; $i++) {
+
+      for ($j = $i + 1; $j < sizeof($game->get_deck()->get_cards()); $j++) {
+
+      if (Card::are_equal_cards($game->get_deck()->get_cards()[$i], $game->get_deck()->get_cards()[$j])) {
+      echo 'HOW IS THAT TWO CARDS ARE EQUAL?!<br><br>';
+      $boolean_flag = true;
+      }
+      }
+      }
+      if (!$boolean_flag) {
+      echo "IT'S OK, THEY'RE ALL DIFFERENT!<br><br>";
+      } */    
